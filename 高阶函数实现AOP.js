a56a074c4c92352f3ada3d98ea805fc9c79297c5
zjx1195688876 <15757116071@163.com>
2017-03-20 00:49:50 +0800

    高阶函数实现AOP

---
+++ 高阶函数实现AOP.js
@@ -0,0 +1,30 @@
+/**
+**高阶函数实现AOP
+**/
+Function.prototype.before = function(beforefn) {
+	var self = this;	//保存原函数引用
+	return function(){	//返回包含了原函数和新函数的 '代理函数'
+		beforefn.apply(this, arguments);	//执行新函数，修正this
+		return self.apply(this,arguments);	//执行原函数
+	}
+}
+
+Function.prototype.after = function(afterfn) {
+	var self = this;
+	return function(){
+		var ret = self.apply(this,arguments);
+		afterfn.apply(this, arguments);
+		return ret;
+	}
+}
+
+var func = function() {
+	console.log('2');
+}
+func = func.before(function() {
+	console.log('1');
+}).after(function() {
+	console.log('3');
+});
+
+func();
