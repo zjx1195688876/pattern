ee9d53ae03768bd5f92b9fff196135e9b6e855f5
zjx1195688876 <15757116071@163.com>
2017-03-26 23:34:37 +0800

    增加单例模式

---
+++ 单例模式.js
@@ -0,0 +1,27 @@
+class CreateUser {
+    constructor(name) {
+        this.name = name;
+        this.getName();
+    }
+
+    getName() {
+         return this.name;
+    }
+}
+
+// 代理实现单例模式
+var ProxyMode = (function() {
+    var instance = null;
+    return function(name) {
+        if(!instance) {
+            instance = new CreateUser(name);
+        }
+        return instance;
+    }
+})();
+// 测试单体模式的实例
+var a = new ProxyMode("aaa");
+var b = new ProxyMode("bbb");
+
+// 因为单体模式是只实例化一次，所以下面的实例是相等的
+console.log(a === b);    //true
