3af5e931a5aa1497b71919b4a0abdb029d3dfb38
zjx1195688876 <15757116071@163.com>
2017-03-31 22:10:18 +0800

    代理模式

---
+++ 代理模式.js
@@ -0,0 +1,22 @@
+var imgFunc = (function() {
+    var imgNode = document.createElement('img');
+    document.body.appendChild(imgNode);
+    return {
+        setSrc: function(src) {
+            imgNode.src = src;
+        }
+    }
+})();
+var proxyImage = (function() {
+    var img = new Image();
+    img.onload = function() {
+        imgFunc.setSrc(this.src);
+    }
+    return {
+        setSrc: function(src) {
+            imgFunc.setSrc('./loading,gif');
+            img.src = src;
+        }
+    }
+})();
+proxyImage.setSrc('./pic.png');
