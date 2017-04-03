9b44e34dc889fbc0cdc7e99d07ba04b8d6c9f18a
zjx1195688876 <15757116071@163.com>
2017-04-03 20:34:35 +0800

    中介者模式

---
+++ 中介者模式.js
@@ -0,0 +1,36 @@
+var goods = {   //手机库存
+    'red|32G': 3,
+    'red|64G': 1,
+    'blue|32G': 7,
+    'blue|32G': 6,
+};
+//中介者
+var mediator = (function() {
+    var colorSelect = document.getElementById('colorSelect');
+    var memorySelect = document.getElementById('memorySelect');
+    var numSelect = document.getElementById('numSelect');
+    return {
+        changed: function(obj) {
+            switch(obj){
+                case colorSelect:
+                    //TODO
+                    break;
+                case memorySelect:
+                    //TODO
+                    break;
+                case numSelect:
+                    //TODO
+                    break;
+            }
+        }
+    }
+})();
+colorSelect.onchange = function() {
+    mediator.changed(this);
+};
+memorySelect.onchange = function() {
+    mediator.changed(this);
+};
+numSelect.onchange = function() {
+    mediator.changed(this);
+};
